1c64cda9779cc57ea0ec5d9d16f69d6c9f3e9dca
Annisyaeka2144 <annisyaekapratiwiaprilia94@gmail.com>
2025-06-18 06:10:09 +0800

    1

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const { connectDB } = require('../config/db');
-const models = require('../models');
+const { connectDB } = require('./config/db');
+const models = require('./models');
 
 const app = express();
 const PORT = process.env.PORT;
@@ -18,10 +18,10 @@ app.use(cors());
 app.use(express.json());
 
 // API Routes
-const authRoutes = require('../routes/auth');
-const karyaRoutes = require('../routes/karya');
-const transaksiRoutes = require('../routes/transaksi');
-const downloadRoutes = require('../routes/download');
+const authRoutes = require('./routes/auth');
+const karyaRoutes = require('./routes/karya');
+const transaksiRoutes = require('./routes/transaksi');
+const downloadRoutes = require('./routes/download');
 
 app.use('/api/auth', authRoutes);
 app.use('/api/karya', karyaRoutes);
@@ -30,14 +30,6 @@ app.use('/api/download', downloadRoutes);
 
 // Test route
 app.get('/test', (req, res) => res.send('Server hidup ✅'));
-
-// Serve frontend
-const clientPath = path.join(__dirname, '../client/build');
-app.use(express.static(clientPath));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(clientPath, 'index.html'));
-});
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
