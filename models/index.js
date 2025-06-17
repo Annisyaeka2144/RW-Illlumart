65930a12fa06a7a6622a07ab95b6cc40e8f63e8c
Annisyaeka2144 <annisyaekapratiwiaprilia94@gmail.com>
2025-06-18 04:29:00 +0800

    final

--- models/index.js
+++ models/index.js
@@ -1,11 +1,13 @@
 const { sequelize } = require('../config/db');
-
 const User = require('./users');
 const Karya = require('./karya');
 const Transaksi = require('./transaksi');
 const Download = require('./download');
 
-// Relasi: Download → Karya
+// Relasi antar-model
+User.hasMany(Karya, { foreignKey: 'userId', as: 'karyaUser' });
+Karya.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+
 Download.belongsTo(Karya, { foreignKey: 'karyaId', as: 'karya' });
 
 module.exports = {
