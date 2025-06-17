4468bb2c636a9ca8896d5642c85a77b97f16da34
Annisyaeka2144 <annisyaekapratiwiaprilia94@gmail.com>
2025-06-18 07:02:28 +0800

    final

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/karya', karyaRoutes);
 app.use('/api/transaksi', transaksiRoutes);
 app.use('/api/download', downloadRoutes);
-
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 // Test route
 app.get('/test', (req, res) => res.send('Server hidup ✅'));
 
